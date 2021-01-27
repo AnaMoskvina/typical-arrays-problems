027dbc8b5fc68d9916fbf374c51ce6b3547d0797
AnaMoskvina <moskvinaanastasia@gmail.com>
2021-01-27 19:04:46 +0300

    write functions min, max, avg

--- src/index.js
+++ src/index.js
@@ -1,12 +1,24 @@
 
 exports.min = function min (array) {
-  return 0;
+  if (!array || !array.length) return 0;
+  let min = array[0];
+  for (let i = 1; i < array.length; i++){
+    min = Math.min(min, array[i]);
+  }
+  return min;
 }
 
 exports.max = function max (array) {
-  return 0;
+  if(!array || !array.length) return 0;
+  let max = array[0];
+  for(let i = 1; i < array.length; i++){
+    max = Math.max(max, array[i]);
+  }
+  return max;
 }
 
 exports.avg = function avg (array) {
-  return 0;
+  if(!array || !array.length) return 0;
+  let sum = array.reduce((acc, item) => acc += item, 0);
+  return sum / array.length;
 }
